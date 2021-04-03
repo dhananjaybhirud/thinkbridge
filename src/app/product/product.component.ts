713edeed69fb958a5f9a1670a49bfa7d2cb1fdf9
dhananjaybhirud <dhananjaybhirud@gmail.com>
2021-04-03 17:56:37 +0530

    edit and delete

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-product',
@@ -7,8 +9,79 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
   @Input('item') item: any;
+  closeResult = '';
+  success = false;
+  product = {};
+  id;
 
-  constructor() {}
+  constructor(
+    private productService: ProductsService,
+    private modalService: NgbModal
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.product = {
+      title: this.item.title,
+      description: this.item.description,
+      price: this.item.price,
+      image: this.item.image,
+      category: this.item.category,
+    };
+  }
+
+  open(content, id) {
+    this.id = id;
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  updateItem(id) {
+    this.productService
+      .updateProduct(id, JSON.stringify(this.product))
+      .subscribe(
+        (data) => {
+          console.log(data);
+          this.success = true;
+          // fetch latest data
+          this.modalService.dismissAll();
+        },
+        (error) => {
+          // this.error = error;
+          // this.loading = false;
+        }
+      );
+  }
+
+  deleteItem(id) {
+    this.productService.deleteProduct(id).subscribe(
+      (data) => {
+        console.log(data);
+        this.success = true;
+        // fetch latest data
+        this.modalService.dismissAll();
+      },
+      (error) => {
+        // this.error = error;
+        // this.loading = false;
+      }
+    );
+  }
 }
